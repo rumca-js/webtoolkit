b57b2851903fbfb93eee5ab263fc46ca2a28661c
Rumpel Stilskin <rozbujnik@gmial.com>
2025-10-21 22:16:15 +0200

    Added handlers without dependencies

--- webtoolkit/handlers/__init__.py
+++ webtoolkit/handlers/__init__.py
@@ -2,3 +2,9 @@
 from .handlerinterface import HandlerInterface
 from .defaulturlhandler import DefaultUrlHandler, DefaultChannelHandler
 from .handlerhttppage import HttpPageHandler
+
+from .handlerchannelyoutube import *
+from .handlervideoyoutube import *
+from .handlerchannelodysee import *
+from .handlervideoodysee import *
+from .handlers import *
