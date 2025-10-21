1eb9fc12cc0227180d754f67101acc8ee1c41916
X Y Z <rozbujnik@gmail.com>
2025-10-21 16:46:07 +0200

    Cleaned up mocks

--- tests/mocks.py
+++ tests/mocks.py
@@ -113,3 +113,94 @@ class YouTubeJsonHandlerMock(YouTubeJsonHandler):
 class YouTubeChannelHandlerMock(YouTubeChannelHandler):
     def input2code_handle(self, url):
         return "UUUUUUUUUUUUUUUU"
+
+
+class MockUrl(object):
+    def __init__(self, url=None, settings=None, url_builder=None):
+        self.url = url
+        self.settings = settings
+        self.url_builder = url_builder
+
+    def get_init_settings(self):
+        return MockCrawler.get_default_crawler(self.url)
+
+    def get_handlers():
+        return []
+
+    def get_type():
+        pass
+
+    def get_contents(self):
+        return ""
+
+    def get_response(self):
+        headers = {}
+        timeout_s = 20
+        self.response = TestResponseObject(self.url, headers, timeout_s)
+        return self.response
+
+    def get_streams(self):
+        pass
+
+    def ping(self, timeout_s=20, user_agent=None):
+        return True
+
+    def get_urls(self):
+        return []
+
+    def get_title(self):
+        pass
+
+    def get_description(self):
+        pass
+
+    def get_language(self):
+        pass
+
+    def get_thumbnail(self):
+        pass
+
+    def get_author(self):
+        pass
+
+    def get_album(self):
+        pass
+
+    def get_tags(self):
+        pass
+
+    def get_date_published(self):
+        pass
+
+    def get_status_code(self):
+        return 200
+
+
+class MockCrawler(CrawlerInterface):
+
+    def run(self):
+        request = self.request
+
+        if self.settings:
+            print("FakeInternet:Url:{} Crawler:{}".format(request.url, self.settings["name"]))
+        else:
+            print("FakeInternet:Url:{}".format(self.request.url))
+
+        MockRequestCounter.requested(request.url, crawler_data=self.settings)
+
+        self.response = TestResponseObject(request.url, request.headers, request.timeout_s)
+
+        return self.response
+
+    def is_valid(self):
+        return True
+
+    def get_default_crawler(url):
+        data = {}
+        data["name"] = "MockCrawler"
+        data["crawler"] = MockCrawler(url = url)
+        data["settings"] = {"timeout_s" : 20}
+
+        return data
+
+
